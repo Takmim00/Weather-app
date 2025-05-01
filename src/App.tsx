7647af43f1413303f2f48925301272631025a28a
Takmim00 <takmimm@gmail.com>
2025-05-01 12:20:30 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-8xl text-center font-bold my-20'>Hi</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths'; // <--- Import this
+
+export default defineConfig({
+  plugins: [react(), tailwindcss(), tsconfigPaths()], // <--- Include it in the plugins array
+});
